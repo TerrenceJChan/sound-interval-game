21e7c58bf172ea8339070880d81a791d00023bfc
Terrence Chan <terrencechan.writer@gmail.com>
2024-10-03 19:29:24 -0400

    Add success and failure messages

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { intervals } from "@/assets/data/intervals";
+import {
+  standardMessagesFailure,
+  standardMessagesSuccess,
+} from "@/assets/data/result-messages/standardMessages";
 import { violinSounds } from "@/assets/data/soundMapping";
 import { Button } from "@/components/ui/button";
+import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
 import DelayedComponent from "../../components/DelayedComponent";
 
 const Game = () => {
+  const [showMainContent, setShowMainContent] = useState(true);
+  const [showResultMessage, setShowResultMessage] = useState(false);
+  const [correct, setCorrect] = useState<null | boolean>(null);
+  const [resultMessage, setResultMessage] = useState("");
+  const [animateOut, setAnimateOut] = useState("");
   const [sound1, setSoundIndex1] = useState<null | {
     note: string;
     sound: string;
@@ -22,11 +32,37 @@ const Game = () => {
   const sound1Ref = useRef<HTMLAudioElement>(null);
   const sound2Ref = useRef<HTMLAudioElement>(null);
 
-  const intervalDifferenceResult = () => {
+  const handleSubmit = () => {
     const result = Math.abs(
       (sound1?.reference ?? 0) - (sound2?.reference ?? 0),
     );
-    return intervals.find((interval) => interval.difference === result)?.name;
+
+    setAnimateOut(
+      "animate-out duration-1000 transition-all ease-in-out fade-out",
+    );
+
+    setTimeout(() => {
+      setShowMainContent(false);
+      setTimeout(() => {
+        if ((inputValue ?? 0) - 1 === result) {
+          setCorrect(true);
+          setResultMessage(
+            standardMessagesSuccess[
+              Math.floor(Math.random() * standardMessagesSuccess.length) - 1
+            ].message,
+          );
+        } else {
+          setCorrect(false);
+          setResultMessage(
+            standardMessagesFailure[
+              Math.floor(Math.random() * standardMessagesFailure.length) - 1
+            ].message,
+          );
+        }
+
+        setShowResultMessage(true);
+      }, 1000);
+    }, 1000);
   };
 
   useEffect(() => {
@@ -52,56 +88,83 @@ const Game = () => {
 
   return (
     <div className="flex flex-grow items-center justify-center">
-      <DelayedComponent delay={100} removeDelay={3000}>
-        <p className="duration-500 animate-in fade-in">
-          Ready? Here comes the first sound.
-        </p>
-      </DelayedComponent>
-      {sound1 && (
-        <DelayedComponent delay={2000}>
-          <audio ref={sound1Ref} autoPlay>
-            <source src={sound1.sound} type="audio/mp3" />
-          </audio>
-        </DelayedComponent>
-      )}
+      {showMainContent && (
+        <div
+          className={clsx(
+            "flex flex-grow items-center justify-center",
+            animateOut,
+          )}
+        >
+          <DelayedComponent delay={100} removeDelay={3000}>
+            <p className="duration-500 animate-in fade-in">
+              Ready? Here comes the first sound.
+            </p>
+          </DelayedComponent>
+          {sound1 && (
+            <DelayedComponent delay={2000}>
+              <audio ref={sound1Ref} autoPlay>
+                <source src={sound1.sound} type="audio/mp3" />
+              </audio>
+            </DelayedComponent>
+          )}
 
-      <DelayedComponent delay={5100} removeDelay={8000}>
-        <p className="duration-500 animate-in fade-in">
-          Here comes the second sound.
-        </p>
-      </DelayedComponent>
-      {sound2 && (
-        <DelayedComponent delay={7000}>
-          <audio ref={sound2Ref} autoPlay>
-            <source src={sound2.sound} type="audio/mp3" />
-          </audio>
-        </DelayedComponent>
-      )}
+          <DelayedComponent delay={5100} removeDelay={8000}>
+            <p className="duration-500 animate-in fade-in">
+              Here comes the second sound.
+            </p>
+          </DelayedComponent>
+          {sound2 && (
+            <DelayedComponent delay={7000}>
+              <audio ref={sound2Ref} autoPlay>
+                <source src={sound2.sound} type="audio/mp3" />
+              </audio>
+            </DelayedComponent>
+          )}
 
-      <DelayedComponent delay={11000}>
-        <div className="flex flex-col items-center gap-8 duration-500 animate-in fade-in">
-          <p>How many semitones were between the two notes?</p>
-          <div className="flex w-full flex-col items-center gap-4">
-            <input
-              onChange={(e) => setInputValue(parseInt(e.target.value))}
-              type="number"
-              placeholder="1-12"
-              min={1}
-              max={12}
-              className="w-1/2 rounded-xl px-4 py-2 text-primary"
-            />
-            <div>
-              {inputValue
-                ? intervals.find(
-                    (interval) => interval.difference === inputValue - 1,
-                  )?.name
-                : ""}
+          <DelayedComponent delay={10000}>
+            <div className="flex flex-col items-center gap-8 duration-500 animate-in fade-in">
+              <p>How many semitones were between the two notes?</p>
+              <div className="flex w-full flex-col items-center gap-4">
+                <input
+                  onChange={(e) => setInputValue(parseInt(e.target.value))}
+                  type="number"
+                  placeholder="1-12"
+                  min={1}
+                  max={12}
+                  className="w-1/2 rounded-xl px-4 py-2 text-primary"
+                />
+                <div>
+                  {inputValue
+                    ? intervals.find(
+                        (interval) => interval.difference === inputValue - 1,
+                      )?.name
+                    : ""}
+                </div>
+              </div>
+
+              <Button onClick={handleSubmit} className="w-1/2 rounded-xl">
+                Submit
+              </Button>
             </div>
-          </div>
-
-          <Button className="w-1/2 rounded-xl">Submit</Button>
+          </DelayedComponent>
         </div>
-      </DelayedComponent>
+      )}
+      {showResultMessage && (
+        <DelayedComponent
+          delay={1000}
+          className="flex flex-col gap-4 duration-1000 animate-in fade-in"
+        >
+          <div
+            className={clsx(
+              "text-4xl",
+              correct ? "text-green-800" : "text-red-800",
+            )}
+          >
+            {correct ? "Correct!" : "Incorrect!"}
+          </div>
+          <div>{resultMessage}</div>
+        </DelayedComponent>
+      )}
     </div>
   );
 };
---
+++ src/assets/data/result-messages/standardMessages.ts
@@ -0,0 +1,45 @@
+export const standardMessagesSuccess = [
+  {
+    id: 0,
+    message: "You are on fire1",
+  },
+  { id: 1, message: "Absolutely nailed it" },
+  { id: 2, message: "Light work 😤" },
+  { id: 3, message: "Keep up the good work" },
+  { id: 4, message: "Slaaaaay!" },
+  {
+    id: 5,
+    message: "Your ears are golden, and your sense of sound is impeccable",
+  },
+  { id: 6, message: "Divine!" },
+  { id: 7, message: "Well done!" },
+  { id: 8, message: "Inspiration work, for sure" },
+  { id: 9, message: "Exceptional beyond any peer" },
+];
+
+export const standardMessagesFailure = [
+  {
+    id: 0,
+    message: "We all make mistakes, shake it off!",
+  },
+  {
+    id: 1,
+    message: "Fall forwards and rise again stronger",
+  },
+  {
+    id: 2,
+    message: "No problem, chin up and go again!",
+  },
+  {
+    id: 3,
+    message: "Why not try again?",
+  },
+  {
+    id: 4,
+    message: "Next time for sure 😤",
+  },
+  {
+    id: 5,
+    message: "Failure begets success, try again!",
+  },
+];
