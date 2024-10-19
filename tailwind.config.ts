4d83f91049216a578a28e311319c3de42ad2093b
Terrence Chan <terrencechan.writer@gmail.com>
2024-10-18 22:55:28 -0400

    feat: Add metadata and favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,9 @@ const config: Config = {
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/page-components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
