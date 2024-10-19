eea0b05f4dd003b9d3355c18cac957ad05ecf9b0
Terrence Chan <terrencechan.writer@gmail.com>
2024-10-18 23:39:24 -0400

    Add clarinet and saxophone sounds

--- src/assets/data/soundMapping.ts
+++ src/assets/data/soundMapping.ts
@@ -1,99 +1,175 @@
 const basePath = "/sound-interval-game";
 
-export const violinSounds = [
+export const sounds = [
   {
     note: "g3",
-    sound: `${basePath}/sounds/violin-g3.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-g3.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-g3.mp3`,
+      violin: `${basePath}/sounds/violin/violin-g3.mp3`,
+    },
     reference: 0,
   },
   {
     note: "a3",
-    sound: `${basePath}/sounds/violin-a3.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-a3.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-a3.mp3`,
+      violin: `${basePath}/sounds/violin/violin-a3.mp3`,
+    },
     reference: 1,
   },
   {
     note: "as3",
-    sound: `${basePath}/sounds/violin-as3.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-as3.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-as3.mp3`,
+      violin: `${basePath}/sounds/violin/violin-as3.mp3`,
+    },
     reference: 2,
   },
   {
     note: "b3",
-    sound: `${basePath}/sounds/violin-b3.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-b3.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-b3.mp3`,
+      violin: `${basePath}/sounds/violin/violin-b3.mp3`,
+    },
     reference: 3,
   },
   {
     note: "c4",
-    sound: `${basePath}/sounds/violin-c4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-c4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-c4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-c4.mp3`,
+    },
     reference: 4,
   },
   {
     note: "cs4",
-    sound: `${basePath}/sounds/violin-cs4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-cs4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-cs4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-cs4.mp3`,
+    },
     reference: 5,
   },
   {
     note: "d4",
-    sound: `${basePath}/sounds/violin-d4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-d4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-d4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-d4.mp3`,
+    },
     reference: 6,
   },
   {
     note: "ds4",
-    sound: `${basePath}/sounds/violin-ds4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-ds4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-ds4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-ds4.mp3`,
+    },
     reference: 7,
   },
   {
     note: "e4",
-    sound: `${basePath}/sounds/violin-e4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-e4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-e4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-e4.mp3`,
+    },
     reference: 8,
   },
   {
     note: "f4",
-    sound: `${basePath}/sounds/violin-f4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-f4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-f4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-f4.mp3`,
+    },
     reference: 9,
   },
   {
     note: "fs4",
-    sound: `${basePath}/sounds/violin-fs4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-fs4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-fs4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-fs4.mp3`,
+    },
     reference: 10,
   },
   {
     note: "g4",
-    sound: `${basePath}/sounds/violin-g4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-g4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-g4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-g4.mp3`,
+    },
     reference: 11,
   },
   {
     note: "a4",
-    sound: `${basePath}/sounds/violin-a4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-a4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-a4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-a4.mp3`,
+    },
     reference: 12,
   },
   {
     note: "as4",
-    sound: `${basePath}/sounds/violin-as4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-as4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-as4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-as4.mp3`,
+    },
     reference: 13,
   },
   {
     note: "b4",
-    sound: `${basePath}/sounds/violin-b4.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-b4.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-b4.mp3`,
+      violin: `${basePath}/sounds/violin/violin-b4.mp3`,
+    },
     reference: 14,
   },
   {
     note: "c5",
-    sound: `${basePath}/sounds/violin-c5.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-c5.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-c5.mp3`,
+      violin: `${basePath}/sounds/violin/violin-c5.mp3`,
+    },
     reference: 15,
   },
   {
     note: "cs5",
-    sound: `${basePath}/sounds/violin-cs5.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-cs5.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-cs5.mp3`,
+      violin: `${basePath}/sounds/violin/violin-cs5.mp3`,
+    },
     reference: 16,
   },
   {
     note: "d5",
-    sound: `${basePath}/sounds/violin-d5.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-d5.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-d5.mp3`,
+      violin: `${basePath}/sounds/violin/violin-d5.mp3`,
+    },
     reference: 17,
   },
   {
     note: "ds5",
-    sound: `${basePath}/sounds/violin-ds5.mp3`,
+    sound: {
+      clarinet: `${basePath}/sounds/clarinet/clarinet-ds5.mp3`,
+      saxophone: `${basePath}/sounds/saxophone/saxophone-ds5.mp3`,
+      violin: `${basePath}/sounds/violin/violin-ds5.mp3`,
+    },
     reference: 18,
   },
 ];
